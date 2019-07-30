2b4fb29040009c2fdb12051cbdc088c40b7c6164
mbbutler <m.brady.butler@gmail.com>
2019-07-30 13:51:45 -0500

    mbb

--- pearc19_model/basic.py
+++ pearc19_model/basic.py
@@ -65,3 +65,45 @@ def pop_dyn(brM = 0.2,    #moose birth rate
         W[i+1] = W[i] + (W[i]*brW*M[i] - W[i]*dfW)*time_step
 
     return t,M,W
+
+
+'''
+Function to run the population dynamics model.
+inputs : parameters for the model
+outputs: time series, Moose pop., Wolf pop. 
+'''
+def pop_dyn_mod(brM = 0.2,    #moose birth rate
+            dfM = 0.003,  #moose death fraction
+            brW = 0.001,  #wolf birth rate
+            dfW = 0.5,    #wolf death fraction
+            ipM = 500,     #initial moose population
+            ipW = 20     #initial wolf population 
+           ):   
+    
+    # Model parameters
+    time_step = 0.0833    # simulation time step in years (a month)
+    start_time = 0        # in years
+    end_time = 50         # in years
+    
+    # Derived constants
+    N = int((end_time - start_time) / time_step)    # number of simulation steps
+    
+    # Time-varying quantities, arrays with one value per time step
+    t = np.zeros(N+1) # time in hours  
+    M = np.zeros(N+1) # moose population  
+    W = np.zeros(N+1) # wolf population
+
+    t[0] = 0          
+    M[0] = ipM
+    W[0] = ipW
+
+    # create a for loop that calculates both moose and wolf populations 
+    # for a given simulation step
+
+    for i in range(N):
+        
+        t[i+1] = t[i] + time_step
+        M[i+1] = M[i] + (M[i]*brM - M[i]*dfM*W[i])*time_step
+        W[i+1] = W[i] + (W[i]*brW*M[i] - W[i]*dfW)*time_step
+
+    return t,M,W
